d7e9e71576505af13f7d9a8293d0238826fb53ea
unknown <osafridi00@gmail.com>
2024-05-06 15:26:08 +0600

    response

--- first-app.js
+++ first-app.js
@@ -1,8 +1,14 @@
 const http = require("http")
 
 const server = http.createServer((req,res)=>{
-console.log(req);
-process.exit()
+console.log(req.url, req.headers, req.method);
+// process.exit()
+res.setHeader("Content-Type", "text/html")
+res.write("<html>")
+res.write("<head><title>NodeJs</title></head>")
+res.write("<body><h1>Hi There This Is node</h1></body>")
+res.write("</html>")
+res.end()
 })
 
 server.listen(3000)
