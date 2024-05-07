79cd1d77a22b269d20d083ffc9dbcbecabf07da0
unknown <osafridi00@gmail.com>
2024-05-07 20:56:01 +0600

    req res

--- first-app.js
+++ first-app.js
@@ -1,16 +1,26 @@
 const http = require("http");
+const fs = require("fs")
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+
+  const method = req.method
 
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
-      "<body><form method='POST'><input type='text'/><button type='submit'>Submit</button></form></body>"
+      "<body><form action='/message' method='POST'><input name='message' type='text'/><button type='submit'>Submit</button></form></body>"
     );
     res.write("</html>");
     return res.end();
+  }
+
+  if(url === '/message' && method == 'POST'){
+    res.statusCode = 302
+    fs.writeFileSync("message.txt","Dummy Text")
+    res.setHeader('Location','/')
+    return res.end()
   }
   // process.exit()
   res.setHeader("Content-Type", "text/html");
